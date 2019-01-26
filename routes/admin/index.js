f1dff2419741d75154bcbbed325c8bdf8cea32de
sogangCSmaster <cs20131575@u.sogang.ac.kr>
2019-01-26 13:55:15 +0900

    need to make admin layout

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const config = require('../../config/config.json');
-const adminId = config.adminID;
-const adminPassword = config.adminPassword;
+const ID = config.adminID;
+const PASSWORD = config.adminPassword;
 
 router.route("/admin")
     .get((req, res, next) => {
-        res.render("admin/index");
+        res.render("admin/login");
+    })
+    .post((req, res, next) => {
+        var { adminId, adminPassword } = req.body;
+        if(adminId==ID && adminPassword==PASSWORD){
+            req.session.adminId = adminId;
+            return res.redirect("/admin/dashboard");
+        }
+        else{
+            var msg = "아이디 혹은 비밀번호가 틀립니다."
+            return res.render("admin/login", { msg });
+        }
     })
 
 
